3e7ed4b5e555a18a9982e8ac8e48a74ea1cafee0
Riccardo Massidda <contact@rmassidda.it>
2018-12-21 11:13:25 +0100

    Comment Drag class

--- js/libs/Drag.js
+++ js/libs/Drag.js
@@ -1,5 +1,6 @@
 class Drag {
   constructor(target) {
+    //  Object to be moved
     this.target = target;
     this.offset = new THREE.Vector3();
     this.intersection = new THREE.Vector3();
@@ -9,18 +10,27 @@ class Drag {
   }
 
   start(tap, camera) {
+    //  Ray generation
     this.raycaster.setFromCamera(tap, camera);
+    //  Plane at the same distance of the object
     let plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), -this.target.position.z);
+    //  Get the intersection of the object and the plane
     if (this.raycaster.ray.intersectPlane(plane, this.intersection)) {
+      //  Inverse matrix used to
       this.inverseMatrix.getInverse(this.target.parent.matrixWorld);
+      //  Offset from the tapped point and the object, in world coordinates
       this.offset.copy(this.intersection).sub(this.worldPosition.setFromMatrixPosition(this.target.matrixWorld));
     }
   }
 
   continue (tap, camera) {
+    //  Ray generation
     this.raycaster.setFromCamera(tap, camera);
+    //  Plane at the same distance of the Object
     let plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), -this.target.position.z);
+    //  Get the intersection of the object and the plane
     if (this.raycaster.ray.intersectPlane(plane, this.intersection)) {
+      //  Move the object considering the previous calculated offset
       this.target.position.copy(this.intersection.sub(this.offset).applyMatrix4(this.inverseMatrix));
     }
   }
